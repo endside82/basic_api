03bd53eeb266459cc7f56c9c48a30a7c0d4cf79b
kyle.unimewo@gmail.com <>
2024-06-26 15:13:05 +0900

    test

--- src/test/java/com/endside/api/stuff/service/StuffServiceTest.java
+++ src/test/java/com/endside/api/stuff/service/StuffServiceTest.java
@@ -47,8 +47,7 @@ class StuffServiceTest {
     @Spy
     StuffMapper stuffMapper = Mappers.getMapper(StuffMapper.class); // 매핑 클래스 null point exception 회피
 
-    @Test
-    void call_getStuff_then_return_stuff() {
+    private Stuff makeDummyStuff() {
         Stuff stuff = Stuff.builder()
                 .name("name")
                 .status(StuffStatus.STANDBY)
@@ -58,67 +57,49 @@ class StuffServiceTest {
         stuff.setId(1L);
         stuff.setCreatedAt(LocalDateTime.now());
         stuff.setUpdatedAt(LocalDateTime.now());
+        return stuff;
+    }
+
+    @Test
+    void call_getStuff_then_return_stuff() {
+        Stuff stuff = makeDummyStuff();
         when(stuffRepository.findById(anyLong())).thenReturn(Optional.of(stuff));
         StuffVo stuffVo = stuffService.getStuff(anyLong());
-        Assertions.assertEquals(stuffVo.getName(),stuff.getName());
-        Assertions.assertEquals(stuffVo.getDescription(),stuff.getDescription());
-        Assertions.assertEquals(stuffVo.getStatus(),stuff.getStatus());
-        Assertions.assertEquals(stuffVo.getMakeDatetime(),stuff.getMakeDatetime());
+        Assertions.assertEquals(stuffVo.getName(), stuff.getName());
+        Assertions.assertEquals(stuffVo.getDescription(), stuff.getDescription());
+        Assertions.assertEquals(stuffVo.getStatus(), stuff.getStatus());
+        Assertions.assertEquals(stuffVo.getMakeDatetime(), stuff.getMakeDatetime());
     }
 
     @Test
     void call_addStuff_then_stuff_added() {
-        Stuff stuff = Stuff.builder()
-                .name("name")
-                .status(StuffStatus.STANDBY)
-                .description("blah blah blah")
-                .makeDatetime(LocalDateTime.now())
-                .build();
-        stuff.setId(1L);
-        stuff.setCreatedAt(LocalDateTime.now());
-        stuff.setUpdatedAt(LocalDateTime.now());
+        Stuff stuff = makeDummyStuff();
         StuffAddParam stuffAddParam = new StuffAddParam();
         when(stuffRepository.save(any(Stuff.class))).thenReturn(stuff);
         StuffVo stuffVo = stuffService.addStuff(stuffAddParam);
-        Assertions.assertEquals(stuffVo.getName(),stuff.getName());
-        Assertions.assertEquals(stuffVo.getDescription(),stuff.getDescription());
-        Assertions.assertEquals(stuffVo.getStatus(),stuff.getStatus());
-        Assertions.assertEquals(stuffVo.getMakeDatetime(),stuff.getMakeDatetime());
+        Assertions.assertEquals(stuffVo.getName(), stuff.getName());
+        Assertions.assertEquals(stuffVo.getDescription(), stuff.getDescription());
+        Assertions.assertEquals(stuffVo.getStatus(), stuff.getStatus());
+        Assertions.assertEquals(stuffVo.getMakeDatetime(), stuff.getMakeDatetime());
     }
 
     @Test
     void call_modStuff_then_stuff_modified() {
-        Stuff stuff = Stuff.builder()
-                .name("name")
-                .status(StuffStatus.STANDBY)
-                .description("blah blah blah")
-                .makeDatetime(LocalDateTime.now())
-                .build();
-        stuff.setId(1L);
-        stuff.setCreatedAt(LocalDateTime.now());
-        stuff.setUpdatedAt(LocalDateTime.now());
+        Stuff stuff = makeDummyStuff();
         StuffModParam stuffModParam = new StuffModParam();
         stuffModParam.setId(1L);
         when(stuffRepository.findById(anyLong())).thenReturn(Optional.of(stuff));
         when(stuffRepository.save(any(Stuff.class))).thenReturn(stuff);
         StuffVo stuffVo = stuffService.modStuff(stuffModParam);
-        Assertions.assertEquals(stuffVo.getName(),stuff.getName());
-        Assertions.assertEquals(stuffVo.getDescription(),stuff.getDescription());
-        Assertions.assertEquals(stuffVo.getStatus(),stuff.getStatus());
-        Assertions.assertEquals(stuffVo.getMakeDatetime(),stuff.getMakeDatetime());
+        Assertions.assertEquals(stuffVo.getName(), stuff.getName());
+        Assertions.assertEquals(stuffVo.getDescription(), stuff.getDescription());
+        Assertions.assertEquals(stuffVo.getStatus(), stuff.getStatus());
+        Assertions.assertEquals(stuffVo.getMakeDatetime(), stuff.getMakeDatetime());
     }
 
     @Test
     void call_getStuffList_then_return_stuff_list() {
-        Stuff stuff = Stuff.builder()
-                .name("name")
-                .status(StuffStatus.STANDBY)
-                .description("blah blah blah")
-                .makeDatetime(LocalDateTime.now())
-                .build();
-        stuff.setId(1L);
-        stuff.setCreatedAt(LocalDateTime.now());
-        stuff.setUpdatedAt(LocalDateTime.now());
+        Stuff stuff = makeDummyStuff();
         StuffVo stuffVo = stuffMapper.updateStuffToVo(stuff);
         Stuff stuff2 = Stuff.builder()
                 .name("name2")
@@ -130,35 +111,31 @@ class StuffServiceTest {
         stuff.setCreatedAt(LocalDateTime.now());
         stuff.setUpdatedAt(LocalDateTime.now());
         StuffVo stuffVo2 = stuffMapper.updateStuffToVo(stuff2);
-        List<StuffVo> stuffList = new ArrayList();
+        List<StuffVo> stuffList = new ArrayList<>();
         stuffList.add(stuffVo);
         stuffList.add(stuffVo2);
-        when(stuffQueryRepository.getStuffListByCondition(any(StuffListParam.class),any(PageRequest.class))).thenReturn(stuffList);
+        when(stuffQueryRepository.getStuffListByCondition(any(StuffListParam.class), any(PageRequest.class))).thenReturn(stuffList);
         StuffListParam stuffListParam = new StuffListParam();
         List<StuffVo> stuffVoList = stuffService.getStuffList(stuffListParam);
-        Assertions.assertEquals(stuffVoList.get(0).getName(),stuff.getName());
-        Assertions.assertEquals(stuffVoList.get(0).getDescription(),stuff.getDescription());
-        Assertions.assertEquals(stuffVoList.get(0).getStatus(),stuff.getStatus());
-        Assertions.assertEquals(stuffVoList.get(0).getMakeDatetime(),stuff.getMakeDatetime());
-        Assertions.assertEquals(stuffVoList.get(1).getName(),stuff2.getName());
-        Assertions.assertEquals(stuffVoList.get(1).getDescription(),stuff2.getDescription());
-        Assertions.assertEquals(stuffVoList.get(1).getStatus(),stuff2.getStatus());
-        Assertions.assertEquals(stuffVoList.get(1).getMakeDatetime(),stuff2.getMakeDatetime());
+        Assertions.assertEquals(stuffVoList.get(0).getName(), stuff.getName());
+        Assertions.assertEquals(stuffVoList.get(0).getDescription(), stuff.getDescription());
+        Assertions.assertEquals(stuffVoList.get(0).getStatus(), stuff.getStatus());
+        Assertions.assertEquals(stuffVoList.get(0).getMakeDatetime(), stuff.getMakeDatetime());
+        Assertions.assertEquals(stuffVoList.get(1).getName(), stuff2.getName());
+        Assertions.assertEquals(stuffVoList.get(1).getDescription(), stuff2.getDescription());
+        Assertions.assertEquals(stuffVoList.get(1).getStatus(), stuff2.getStatus());
+        Assertions.assertEquals(stuffVoList.get(1).getMakeDatetime(), stuff2.getMakeDatetime());
     }
 
     @Test
     void call_retrieveStuff_then_get_stuff_() {
-        Stuff stuff = Stuff.builder()
-                .name("name")
-                .status(StuffStatus.STANDBY)
-                .description("blah blah blah")
-                .makeDatetime(LocalDateTime.now())
-                .build();
+        Stuff stuff = makeDummyStuff();
         when(stuffRepository.findById(anyLong())).thenReturn(Optional.of(stuff));
-        Stuff returnStuff =  stuffService.retrieveStuff(1L);
-        Assertions.assertEquals(returnStuff.getName(),stuff.getName());
-        Assertions.assertEquals(returnStuff.getDescription(),stuff.getDescription());
-        Assertions.assertEquals(returnStuff.getStatus(),stuff.getStatus());
-        Assertions.assertEquals(returnStuff.getMakeDatetime(),stuff.getMakeDatetime());
+        Stuff returnStuff = stuffService.retrieveStuff(1L);
+        Assertions.assertEquals(returnStuff.getName(), stuff.getName());
+        Assertions.assertEquals(returnStuff.getDescription(), stuff.getDescription());
+        Assertions.assertEquals(returnStuff.getStatus(), stuff.getStatus());
+        Assertions.assertEquals(returnStuff.getMakeDatetime(), stuff.getMakeDatetime());
     }
+
 }
